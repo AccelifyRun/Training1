6edc035bcf704e3b42a628993c28008583b09612
Iakov Volf <jakoff@gmail.com>
2016-07-21 20:35:44 +0300

    Created class Page

---
+++ src/test/java/Telran/com/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package Telran.com.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Iakov on 7/21/2016.
+ */
+public class LoginPage extends Page {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+}
