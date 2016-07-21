9ea35ccabc28415044101a1618fe38f8025aa2b8
tan3su <tan3su@gmail.com>
2016-07-21 20:41:41 +0300

    Merge branch 'master' of C:\Users\Boss\Training1 with conflicts.

--- src/test/java/Telran/com/pages/LoginPage.java
+++ src/test/java/Telran/com/pages/LoginPage.java
@@ -1,13 +1,10 @@
 package Telran.com.pages;
 
-import org.openqa.selenium.WebDriver;
-
 /**
- * Created by Iakov on 7/21/2016.
+ * Created by Boss on 21.07.2016.
  */
-public class LoginPage extends Page {
+public class LoginPage extends Page{
 
-    public LoginPage(WebDriver driver) {
-        super(driver);
-    }
+
+
 }
