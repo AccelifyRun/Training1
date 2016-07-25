0b955fc17985ae0266f22822b30d2f531980f8cc
Iakov Volf <jakoff@gmail.com>
2016-07-25 20:03:26 +0300

    Created packadge for trainig

--- src/test/java/Telran/com/Training/LoginPageAndreyMarkov.java
+++ src/test/java/Telran/com/Training/LoginPageAndreyMarkov.java
@@ -1,5 +1,6 @@
-package Telran.com.pages;
+package Telran.com.Training;
 
+import Telran.com.pages.Page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/test/java/Telran/com/pages/LoginPage.java
+++ src/test/java/Telran/com/pages/LoginPage.java
@@ -1,13 +1,23 @@
 package Telran.com.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 
 /**
  * Created by Iakov on 7/21/2016.
  */
 public class LoginPage extends Page {
 
+    @FindBy(id = "MainContent_LoginUser_UserName")
+    WebElement usernameField;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_firstNameTxt")
+    WebElement firstName;
+
     public LoginPage(WebDriver driver) {
         super(driver);
     }
+
+
 }
