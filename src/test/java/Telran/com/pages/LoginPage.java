d38bfc4b1e90d2fb43e1873e1a12811246ad6885
Iakov Volf <jakoff@gmail.com>
2016-07-25 21:02:22 +0300

    Added methods

--- src/test/java/Telran/com/pages/LoginPage.java
+++ src/test/java/Telran/com/pages/LoginPage.java
@@ -3,6 +3,7 @@ package Telran.com.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 
 /**
@@ -14,10 +15,23 @@ public class LoginPage extends Page {
     WebElement usernameField;
     @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_firstNameTxt")
     WebElement firstName;
+    @FindBy(id = "MainContent_AddNewUser")
+    WebElement AddNewUser;
 
     public LoginPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
+
+    public void fillusernameField(String username) {
+
+        setElementText(usernameField, username);
+
+    }
+
+    public void ClickToAddNewUser() {
+        clickElement(AddNewUser);
+    }
 
 }
