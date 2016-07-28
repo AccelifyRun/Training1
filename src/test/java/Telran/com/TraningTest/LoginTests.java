963a7328a046c82c92dccbf83c920d3b74121b2a
Iakov Volf <jakoff@gmail.com>
2016-07-28 20:56:48 +0300

    Created class Page

---
+++ src/test/java/Telran/com/TraningTest/LoginTests.java
@@ -0,0 +1,54 @@
+package Telran.com.TraningTest;
+
+import Telran.com.Training.LoginAlicePage;
+import Telran.com.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+//@org.testng.annotations.Listeners(FailTestScreenshotListener.class)
+public class LoginTests {
+    // private static Logger Log = Logger.getLogger(LogLog4j.class.getName());
+    public static String registered_username = "3339Doctor";
+    public static String registered_password = "LinkCare!!11";
+    public LoginPage loginPage;
+    public LoginAlicePage loginAlicePage;
+    public WebDriver driver;
+
+    @BeforeClass(alwaysRun = true)
+    public void setup() {
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginAlicePage = PageFactory.initElements(driver, LoginAlicePage.class);
+        driver = new ChromeDriver();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethodSetUp() {
+
+
+    }
+
+    @Test
+    public void testLoginWithExtData() {
+        driver.get("http://dhclinicappv2stg.item-soft.co.il");
+        // Log.info("TestLoginWithExtData was started....");
+        loginPage.clickToAddNewUser();
+        loginPage.fillusernameField("Username");
+        loginAlicePage.fillUserFirstnameField("FirstName");
+        loginAlicePage.clickDropdownNameField("2");
+        // Assert.assertTrue(loginPage.isLoginUnsuccessfulAlertMessageDisplayed());
+        // Log.info("TestLoginWithExtData stoped....");
+    }
+
+
+    // test of clicking on Registration link are written in another class
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        this.driver.quit();
+    }
+}
