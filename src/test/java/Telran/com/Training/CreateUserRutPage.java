b2f6d251269811147d917575515dece809a40cda
RutGalchuk <rut.galchuk@gmail.com>
2016-08-01 18:53:22 +0300

    some changes in CreateUserRutPage

--- src/test/java/Telran/com/Training/CreateUserRutPage.java
+++ src/test/java/Telran/com/Training/CreateUserRutPage.java
@@ -47,6 +47,8 @@ public class CreateUserRutPage extends Page {
     WebElement addUserButton;
     @FindBy(id = "MainContent_LinkButton1")
     WebElement cancellationButton;
+    @FindBy(id = "ctl00_MainContent_RegisterUser_CreateUserStepContainer_birthdayTxt_CalendarPopupButton")
+    WebElement calendarButton;
 
     //dropdowns
     @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_rolesDDl")
@@ -60,10 +62,57 @@ public class CreateUserRutPage extends Page {
     @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_IsClinicAdmin")
     WebElement clinicAdminCheckbox;
 
+    //warnings
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_UserNameRequired")
+    WebElement userNameWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_FirsNameRequired")
+    WebElement firstNameWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_LastNameRequired")
+    WebElement lastNameWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_EmailRequired")
+    WebElement emailWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_PasswordRequired")
+    WebElement passwordWarning; //15
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_ConfirmPasswordRequired")
+    WebElement confirmPasswordWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RequiredFieldValidator1")
+    WebElement idWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RequiredFieldValidator2")
+    WebElement mobilePhoneWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RequiredFieldValidator3")
+    WebElement streetWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RequiredFieldValidator5")
+    WebElement houseWarning;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RequiredFieldValidator4")
+    WebElement cityWarning;
+
+    //errors
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RegularExpressionValidator2")
+    WebElement usernameError;   //15, numbers or letters
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RegularExpressionValidator1")
+    WebElement firstNameError;  //15, only letters
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RegularExpressionValidator3")
+    WebElement lastNameError;   //15, only letters
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_regexEmailValid")
+    WebElement mailError;       //not valid
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_PasswordCompare")
+    WebElement confirmPasswordError;    //not valid
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_CVPersonalId")
+    WebElement idError;
+    @FindBy(xpath = "//*[@class='riTextBox riError']")
+    WebElement birthdayError;
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RegularExpressionValidator4")
+    WebElement streetError;   //30, only letters and numbers,  not simbols
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RegularExpressionValidator5")
+    WebElement houseError;  //10, not simbols, only letters and numbers
+    @FindBy(id = "MainContent_RegisterUser_CreateUserStepContainer_RegularExpressionValidator6")
+    WebElement cityError;    //15, only letters and numbers, not simbols
+    @FindBy(id = "")
+    WebElement Error;
+
     public CreateUserRutPage(WebDriver driver) {
         super(driver);
     }
-
     public void CreateUserRutPage() {
         driver.get("http://dhclinicappv2stg.item-soft.co.il/SitePages/createUser.aspx?ReturnUrl=HomePage");
     }
@@ -72,27 +121,21 @@ public class CreateUserRutPage extends Page {
     public void fillUsernameField(String username) {
         setElementText(usernameField, username);
     }
-
     public void fillFirstNameField(String firstName) {
         setElementText(firstNameField, firstName);
     }
-
     public void fillLastNameField(String lastName) {
         setElementText(lastNameField, lastName);
     }
-
     public void fillMailField(String mail) {
         setElementText(mailField, mail);
     }
-
     public void fillPasswordField(String password) {
         setElementText(passwordField, password);
     }
-
     public void fillConfirmPasswordField(String password) {
         setElementText(confirmPasswordField, password);
     }
-
     public void filllDField(String id) {
         setElementText(idField, id);
     }
@@ -101,31 +144,24 @@ public class CreateUserRutPage extends Page {
     public void fillBirthdayField(String birthday) {
         setElementText(birthdayField, birthday);
     }
-
     public void fillMobilePhoneField(String mobilePhone) {
         setElementText(mobilePhoneField, mobilePhone);
     }
-
     public void fillPhoneField(String phone) {
         setElementText(phoneField, phone);
     }
-
     public void fillStreetField(String street) {
         setElementText(streetField, street);
     }
-
     public void fillHouseField(String house) {
         setElementText(houseField, house);
     }
-
     public void fillCityField(String city) {
         setElementText(cityField, city);
     }
-
     public void fillCountryField(String country) {
         setElementText(countryField, country);
     }
-
     public void fillClinicNameField(String clinicName) {
         setElementText(clinicNameField, clinicName);
     }
@@ -140,7 +176,6 @@ public class CreateUserRutPage extends Page {
     public void clickOnAddUserButton() {
         clickElement(addUserButton);
     }
-
     public void clickOnCancellationButton() {
         clickElement(cancellationButton);
     }
